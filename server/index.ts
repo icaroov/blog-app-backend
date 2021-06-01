cc55560397eef041407ad1b7a7957b01be7e932f
Icaro Oliveira <icarovinici@gmail.com>
2021-06-01 13:36:11 -0300

    chore: create initial structure

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import dontenv from 'dotenv'
+dontenv.config()
+
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+
+const server = express()
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
+server.use(cors())
+server.use(morgan('dev'))
+server.use(cookieParser())
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log('⚡ Server is running on http://localhost:5000.')
+})
