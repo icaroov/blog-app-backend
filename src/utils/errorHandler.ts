9545478e9440987c40ce872c5bda250754f98d4c
Icaro Oliveira <icarovinici@gmail.com>
2021-06-01 15:52:20 -0300

    feat: add error handler helper

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express'
+
+class ErrorHandler extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string) {
+    super()
+    this.statusCode = statusCode
+    this.message = message
+  }
+}
+
+const handleError = (err: ErrorHandler, res: Response) => {
+  const { statusCode, message } = err
+
+  res.status(statusCode).json({
+    status: 'ERROR',
+    statusCode,
+    message,
+  })
+}
+
+export { ErrorHandler, handleError }
